13f64e7cd3e3249be46628f641a260d35bc7d052
pierallard <pierallard@gmail.com>
2019-04-27 18:47:10 +0200

    Coin

--- src/game/PlayableCoin.ts
+++ src/game/PlayableCoin.ts
@@ -17,7 +17,11 @@ export class PlayableCoin {
   }
 
   create(game: Phaser.Game, group: Phaser.Group) {
-    this.sprite = game.add.sprite(this.position.x * TILE_SIZE, this.position.y * TILE_SIZE, 'chips', 36);
+    this.sprite = game.add.sprite(this.position.x * TILE_SIZE, this.position.y * TILE_SIZE, 'coin');
+    this.sprite.animations.add('IDLE', [0, 1, 2], Phaser.Timer.SECOND / 100, true);
+    this.sprite.animations.add('RUN', [3, 4, 5, 6, 7, 8], Phaser.Timer.SECOND / 50, true);
+
+    this.sprite.animations.play('IDLE');
     group.add(this.sprite);
 
     this.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
@@ -43,12 +47,26 @@ export class PlayableCoin {
       this.moveTo(game, this.position.up(), level);
     } else if (this.downKey.isDown) {
       this.moveTo(game, this.position.down(), level);
+    } else {
+      if (this.sprite.animations.currentAnim.name !== 'IDLE') {
+        this.sprite.animations.play('IDLE');
+      }
     }
   }
 
   private moveTo(game: Phaser.Game, position: Point, level) {
     if (!this.isMovingAllowed(position, level)) {
       return;
+    }
+    if (this.sprite.animations.currentAnim.name !== 'RUN') {
+      this.sprite.animations.play('RUN');
+    }
+    if (this.position.x > position.x) {
+      this.sprite.scale.set(-1, 1);
+      this.sprite.anchor.set(1, 0);
+    } else {
+      this.sprite.scale.set(1, 1);
+      this.sprite.anchor.set(0, 0);
     }
     this.isMoving = true;
     this.position.x = position.x;
--- src/game/game_state/Preload.ts
+++ src/game/game_state/Preload.ts
@@ -21,6 +21,7 @@ export default class Preload extends Phaser.State {
         this.game.load.spritesheet('normal_hero', 'dist/assets/images/gentil_hero_all.png', 30, 30);
         this.game.load.spritesheet('evil_hero', 'dist/assets/images/evil_hero_all.png', 30, 30);
         this.game.load.spritesheet('normal_coin', 'dist/assets/images/gentil_coin.png', 24, 24);
+        this.game.load.spritesheet('coin', 'dist/assets/images/coin.png', 24, 24);
     }
 
     private loadFonts() {

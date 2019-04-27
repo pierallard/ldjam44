5e564bd4b0f322194a4021ace2c6e2ff18d69787
Paul Chasle <paul.chasle@akeneo.com>
2019-04-27 21:02:15 +0200

    Cleanup stage update

--- src/game/game_state/Stage1.ts
+++ src/game/game_state/Stage1.ts
@@ -1,8 +1,11 @@
 import { Level1 } from "../../levels/Level1";
 import { Stage } from "./Stage";
+import { Game, StateManager } from "phaser-ce";
 
 export default class Stage1 extends Stage {
   constructor() {
     super(new Level1());
   }
+
+  onStageEnd = () => this.game.state.start("Stage2");
 }

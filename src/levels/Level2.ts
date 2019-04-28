d51e5367e75d89b13fc53dcb56c37c2b18a9a812
Adrien Pétremann <hello@grena.fr>
2019-04-28 11:50:33 +0200

    update levels

--- src/levels/Level2.ts
+++ src/levels/Level2.ts
@@ -4,17 +4,30 @@ import Point from "../game/Point";
 export class Level2 extends Level {
   constructor() {
     super([
+        [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 1, 0, 1, 0, 0, 1, 0],
+        [0, 0, 1, 1, 0, 1, 1, 0, 1, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 1, 1, 0, 1, 1, 0, 1, 0],
+        [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 1, 1, 0],
+        [0, 1, 0, 1, 1, 0, 0, 1, 1, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
       ],
-      new Point(0, 0),
-      [
-        new Point(1, 1)
+      new Point(5, 5), // hero
+      [ // coins
+        new Point(1, 1),
+        new Point(10, 1),
+        new Point(5, 2),
+        new Point(2, 5),
+        new Point(8, 5),
+        new Point(10, 5),
+        new Point(5, 8),
+        new Point(3, 10),
+        new Point(10, 10),
       ],
-      new Point(4, 3)
+      new Point(7, 7) // playable coin
     );
   }
 }
